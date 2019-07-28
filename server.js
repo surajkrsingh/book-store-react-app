bffc76fa61f1104271039ff1e260d8574c8c7951
surajkrsingh <suraj.sk243@gmail.com>
2019-07-28 21:13:03 +0530

    Finished deployment

--- server.js
+++ server.js
@@ -1,21 +1,67 @@
 #!/usr/bin/env node
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var mongoose = require('mongoose');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-/**
- * Module dependencies.
- */
+//MOngodb connect
+mongoose.connect('mongodb://localhost/node-graphql', { promiseLibrary: require('bluebird'), useNewUrlParser: true })
+  .then(() =>  console.log('connection successful'))
+  .catch((err) => console.error(err));
 
-var app = require('app');
+var app = express();
+
+//GraphQL
+var graphqlHTTP = require('express-graphql');
+var schema = require('./graphql/bookSchemas');
+var cors = require("cors");
+app.use('*', cors());
+app.use('/graphql', cors(), graphqlHTTP({
+  schema: schema,
+  rootValue: global,
+  graphiql: true,
+}));
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
 var debug = require('debug')('server:server');
 var http = require('http');
-const path = require('path');
 
-app.use(express.static('public'));
-app.get('*',(req,res) => {
-	res.sendFile(path.resolve(__dirname, 'public', 'index.html' ));
-});
 /**
  * Get port from environment and store in Express.
  */
+
 var port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
